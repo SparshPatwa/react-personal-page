256b3ca78ddb0e6eb23f6dc899151f21f387c3e6
Sparsh Patwa <patwa.sparsh@gmail.com>
2020-10-25 16:52:23 -0700

    Fix page bugs

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,15 +1,16 @@
 import React from 'react';
-/*
 import Hero from '../components/Hero';
-import Carousel from '../components/Carousel';
-*/
-function AboutPage(props) {
+import Content from '../components/Content';
 
+function AboutPage(props) {
     return(
         <div>
-            About Page Rendered
+            <Hero title={props.title} />
+            <Content>
+            <p>Hi, my name is Sparsh Patwa. I'm currently a hardware silicon engineer at Alphabet working on the bleeding edge of memory and compute for Ambient Computing and Machine Learning Acceleration with a love for hardware-software co-design.</p>
+            <p>I have recently ventured into the world of fullstack software development and hope to bring the same passion and innovating thinking that I do to hardware silicon engineering.</p>
+            </Content>
         </div>
     );
-
 }
 export default AboutPage;

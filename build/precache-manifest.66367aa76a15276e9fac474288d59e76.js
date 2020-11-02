b1c93275066d30edbc740cc41e2dbaaa346e06c3
Sparsh Patwa <patwa.sparsh@gmail.com>
2020-11-01 21:26:08 -0800

    Fix Heroku deployment

---
+++ build/precache-manifest.66367aa76a15276e9fac474288d59e76.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6219dcd6929cb34cd4d877a2326e7d47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eea3f7c8cbbcd843b62d",
+    "url": "/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "6e94260ca5bf343db0a1",
+    "url": "/static/css/main.1906f31c.chunk.css"
+  },
+  {
+    "revision": "eea3f7c8cbbcd843b62d",
+    "url": "/static/js/2.7860e127.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.7860e127.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e94260ca5bf343db0a1",
+    "url": "/static/js/main.7ae86336.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "d74a1de34fba206e13ba6bdfd8e3610a",
+    "url": "/static/media/dory.d74a1de3.jpg"
+  },
+  {
+    "revision": "938af4d306a4aebfdb18c482904e2762",
+    "url": "/static/media/github.938af4d3.jpg"
+  },
+  {
+    "revision": "6f851efb44210286ab613c674ea0be6f",
+    "url": "/static/media/health.6f851efb.jpg"
+  },
+  {
+    "revision": "8e3ddff644630a29d1fdb02f859eb759",
+    "url": "/static/media/hike.8e3ddff6.jpg"
+  }
+]);

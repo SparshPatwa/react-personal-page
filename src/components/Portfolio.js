7fb42be4ccee8cc8abffc01dfc2327ab6e2fce5a
Sparsh Patwa <patwa.sparsh@gmail.com>
2020-11-01 18:48:45 -0800

    Add new fullstack project to portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,6 +5,7 @@ import Card from './Card';
 
 import hikefinder from "../assets/images/hike.jpg";
 import healthtracker from "../assets/images/health.jpg";
+import dory from "../assets/images/dory.jpg";
 import githubrepo from "../assets/images/github.jpg";
 
 class Portfolio extends React.Component {
@@ -15,20 +16,21 @@ class Portfolio extends React.Component {
             items: [
                 {
                     id: 0,
+                    title: 'Dory',
+                    subTitle: 'A Q&A application deployed using full-stack technologies ',
+                    imgSrc: dory,
+                    link: 'https://dory-polling.herokuapp.com/',
+                    selected: false
+                },
+                {
+                    id: 1,
                     title: 'Hike Finder',
                     subTitle: 'A hike planner application deployed using front-end technologies',
                     imgSrc: hikefinder,
                     link: 'https://jennifermulder.github.io/explore/',
                     selected: false
                 },
-                {
-                    id: 1,
-                    title: 'GitHub',
-                    subTitle: 'My GitHub page with repositories and projects I\'ve worked on',
-                    imgSrc: githubrepo,
-                    link: 'https://github.com/SparshPatwa',
-                    selected: false
-                },
+                
                 {
                     id: 2,
                     title: 'Health Tracker',
@@ -36,7 +38,16 @@ class Portfolio extends React.Component {
                     imgSrc: healthtracker,
                     link: 'https://health-track-it.herokuapp.com/about',
                     selected: false
-                }   
+                },
+
+                {
+                    id: 3,
+                    title: 'GitHub',
+                    subTitle: 'My GitHub page with repositories and projects I\'ve worked on',
+                    imgSrc: githubrepo,
+                    link: 'https://github.com/SparshPatwa',
+                    selected: false
+                }
             ]
         }
     }
